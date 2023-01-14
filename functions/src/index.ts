1c175560e67202910e17048108c2ec8e3c69a8e2
Kohei Tanaka <kohei69@gmail.com>
2023-01-14 21:22:59 +0900

    add

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,22 @@
-const message = 'aaa'
+import slack from 'slack';
+import { PubsubMessage } from "@google-cloud/pubsub/build/src/publisher";
 
-console.log(message)
+const BOT_ACCESS_TOKEN = process.env.BOT_ACCESS_TOKEN
+const CHANNEL = process.env.SLACK_CHANNEL || 'bot';
+
+// TODO: pubsubEvent type
+exports.notifySlack = async (pubsubEvent: any) => {
+  const pubsubAttrs = pubsubEvent.attributes;
+  const pubsubData = Buffer.from(pubsubEvent.data, 'base64').toString();
+  const budgetNotificationText = `${JSON.stringify(
+    pubsubAttrs
+  )}, ${pubsubData}`;
+
+  await slack.chat.postMessage({
+    token: BOT_ACCESS_TOKEN,
+    channel: CHANNEL,
+    text: budgetNotificationText,
+  });
+
+  return 'Slack notification sent successfully';
+};
